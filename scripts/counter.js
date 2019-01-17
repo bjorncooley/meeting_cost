d23b51bb59f87345a15ec1d1a8ba26b2fa4aae1c
Bjorn Cooley <bjorncooley@gmail.com>
2019-01-17 15:30:41 -0800

    set initial cost

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,14 +2,15 @@ let averageSalary = 150000;
 let numParticipants = 10;
 let workingDays = 260;
 let costPerMinute = Math.ceil(averageSalary / workingDays / 8 / 60 * numParticipants);
-let currentCost = 0;
+let currentCost = costPerMinute;
 
 $(window).load(function() {
+  displayCost();
   incrementCost();
 });
 
 function incrementCost() {
-  const incrementInSeconds = 30;
+  const incrementInSeconds = 20;
   const timeInterval = incrementInSeconds * 1000;
   const costInterval = costPerMinute / (60 / incrementInSeconds);
 
